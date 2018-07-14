14a5b6ec483ac873ebf2100e4e4adca5088c8ec0
Victor Frazao <31864869+vfrazao-ns1@users.noreply.github.com>
2018-07-14 16:21:28 -0400

    reorganize test structure

--- IEX_hist_parser/tests/test_parser.py
+++ IEX_hist_parser/tests/test_parser.py
@@ -1,39 +1,47 @@
+import os
+import sys
 import unittest
-from . import IEXparser
+import IEX_hist_parser.IEXparser as iex
+
+if sys.argv[0]:
+    os.chdir(os.path.dirname(sys.argv[0]))
 
 
 class ParserTestCases(unittest.TestCase):
     """
     Tests for parser.py
     """
+
+    def setUp(self):
+        self.test_file = 'input_files\\example1.pcap'
+        self.p = iex.Parser(self.test_file)
+
+    def tearDown(self):
+        self.p.file.close()
+
     def test_file_load(self):
         """
         Tests the functionality of the records file reader for normal
         functionality (loads a file and doesn't raise an exception)
         """
-        test_file = 'input_files\\example1.pcap'
-        p = IEXparser.Parser(test_file)
 
         self.assertEqual(
             1,
             1,
-            msg=f'Test with {test_file}'
+            msg=f'Test with {self.test_file}'
         )
 
     def test_seek_header(self):
         """
         Tests functions ability to find the TP header
         """
-        test_file = 'input_files\\example1.pcap'
+        self.p._seek_header()
 
-        p = IEXparser.Parser(test_file)
-        p._seek_header()
+        self.assertEqual(self.p.bytes_read, 1930)
 
-        self.assertEqual(p.bytes_read, 1930)
-    
     def test_end_to_end(self):
         test_file = 'input_files\\example1.pcap'
-        with IEXparser.Parser(test_file) as p:
+        with iex.Parser(test_file) as p:
             for message in p:
                 pass
 
