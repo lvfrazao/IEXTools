93802ffecbf52684030bcb58c02940557320bf75
Victor Frazao <31864869+vfrazao-ns1@users.noreply.github.com>
2018-11-05 22:16:32 -0500

    Updated analysis of PCAP files

---
+++ IEX_hist_parser/analysis/updated_hourly_analysis.py
@@ -0,0 +1,5 @@
+import hourly_pcap_analysis
+
+if __name__ == "__main__":
+    file_path = r"C:\Users\luiz_\Dropbox\Personal\Python\Programs\IEX_Examples\IEX_data\20181101_IEXTP1_TOPS1.6.pcap"
+    hourly_pcap_analysis.hourly_message_distribution(file_path)
